fa03716b478e3dbc91c0b604d67cd02d9dc9865c
Tien Thanh <notnighter@gmail.com>
2025-11-12 11:17:30 +0700

    add: first commit

---
+++ src/main/java/github/nighter/pluginLangCore/PluginLangCore.java
@@ -0,0 +1,17 @@
+package github.nighter.pluginLangCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginLangCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
